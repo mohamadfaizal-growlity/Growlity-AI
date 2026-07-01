d266bfa961743f4d85f90a964b5dee307ab44c9f
mohamadfaizal-growlity <mahamadfaizal.growlity@gmail.com>
2026-07-01 18:18:33 +0530

    fix: downgrade @astrojs/vercel to v8 to fix astro 5 build compatibility on vercel

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,34 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_VosEtthh.mjs';
+import { manifest } from './manifest_AdBqLx27.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/api/contact.astro.mjs');
+const _page2 = () => import('./pages/growlity-secret-panel.astro.mjs');
+const _page3 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/api/contact.ts", _page1],
+    ["src/pages/growlity-secret-panel.astro", _page2],
+    ["src/pages/index.astro", _page3]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./noop-entrypoint.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "9da6e9f5-1909-41fc-b3ff-0f2f5d88da46",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (Object.prototype.hasOwnProperty.call(serverEntrypointModule, _start)) ;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
---
+++ .vercel/output/_functions/pages/_image.astro.mjs
@@ -0,0 +1,2 @@
+export { a as page } from '../chunks/generic_CZ8aW5Qi.mjs';
+export { renderers } from '../renderers.mjs';
